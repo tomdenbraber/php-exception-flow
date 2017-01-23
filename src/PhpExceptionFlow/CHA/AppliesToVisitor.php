061d4c60d03b88d5713e0bdad5aad5a0bf7b7088
Tom den Braber <tomdenbraber@gmail.com>
2017-01-23 14:52:17 +0100

    add partial order visitor which calculates the applies-to sets and creates a class-to-method maps

---
+++ src/PhpExceptionFlow/CHA/AppliesToVisitor.php
@@ -0,0 +1,37 @@
+<?php
+namespace PhpExceptionFlow\CHA;
+use PhpExceptionFlow\Collection\PartialOrder\PartialOrderVisitorInterface;
+
+class AppliesToVisitor implements PartialOrderVisitorInterface {
+
+	/** @var AppliesToCalculatorInterface $partial_order */
+	private $applies_to_calculator;
+
+	/**
+	 * @var array $class_to_method_map
+	 */
+	private $class_to_method_map = [];
+
+
+	public function __construct(AppliesToCalculatorInterface $applies_to_calculator) {
+		$this->applies_to_calculator = $applies_to_calculator;
+	}
+
+	/**
+	 * @param Method $method
+	 */
+	public function visitElement($method) {
+		$method_applies_to = $this->applies_to_calculator->calculateAppliesTo($method);
+
+		foreach ($method_applies_to as $class) {
+			if (isset($this->class_to_method_map[$class]) === false) {
+				$this->class_to_method_map[$class] = [$method->getName() => $method];
+			}
+			$this->class_to_method_map[$class][$method->getName()] = $method;
+		}
+	}
+
+	public function getClassToMethodMap() {
+		return $this->class_to_method_map;
+	}
+}
