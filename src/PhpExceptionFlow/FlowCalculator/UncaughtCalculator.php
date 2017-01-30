c1a32677e6c423e86324cc980326a745478a5299
Tom den Braber <tomdenbraber@gmail.com>
2017-01-30 11:34:46 +0100

    made uncaughtcalculator work, incl tests

--- src/PhpExceptionFlow/FlowCalculator/UncaughtCalculator.php
+++ src/PhpExceptionFlow/FlowCalculator/UncaughtCalculator.php
@@ -1,11 +1,10 @@
 <?php
 namespace PhpExceptionFlow\FlowCalculator;
 
-use PhpExceptionFlow\EncountersCalculator;
 use PhpExceptionFlow\GuardedScope;
 use PhpExceptionFlow\Scope;
+use PhpExceptionFlow\ScopeVisitor\CaughtExceptionTypesCalculator;
 use PhpParser\Node\Stmt\Catch_;
-use PHPTypes\State;
 use PHPTypes\Type;
 
 class UncaughtCalculator implements ExceptionSetCalculatorInterface {
@@ -15,55 +14,61 @@ class UncaughtCalculator implements ExceptionSetCalculatorInterface {
 	/** @var Type[][]|\SplObjectStorage */
 	private $guarded_scopes;
 
-	/** @var \SplObjectStorage */
+	/**
+	 * @var \SplObjectStorage $catch_clause_catches
+	 * this contains actual instances of exceptions that are caught by the catch clause, not the types that it resolves
+	 */
 	private $catch_clause_catches;
 
 	/** @var CaughtExceptionTypesCalculator $catch_clause_type_resolver */
 	private $catch_clause_type_resolver;
 
-	/** @var EncountersCalculator */
+	/** @var CombiningCalculator */
 	private $encounters_calculator;
 
-	/** @var State $state */
-	private $state;
-
-	public function __construct(State $state, CaughtExceptionTypesCalculator $catch_clause_type_resolver, EncountersCalculator $encounters_calculator) {
+	public function __construct(CaughtExceptionTypesCalculator $catch_clause_type_resolver, CombiningCalculator $encounters_calculator) {
 		$this->scopes = new \SplObjectStorage;
 		$this->guarded_scopes = new \SplObjectStorage;
 		$this->catch_clause_catches = new \SplObjectStorage;
-		$this->state = $state;
 		$this->catch_clause_type_resolver = $catch_clause_type_resolver;
 		$this->encounters_calculator = $encounters_calculator;
 	}
 
-	public function leaveScope(Scope $scope) {
-		$uncaught = [];
-		foreach ($scope->getGuardedScopes() as $guarded_scope) {
-			$uncaught = array_merge($uncaught, $this->getForGuardedScope($guarded_scope));
+	public function determineForScope(Scope $scope) {
+		$guarded_scopes = $scope->getGuardedScopes();
+		$uncaught = array();
+		foreach ($guarded_scopes as $guarded_scope) {
+			$inclosed_encounters = $this->encounters_calculator->getForScope($guarded_scope->getInclosedScope());
+			$catch_clauses = $guarded_scope->getCatchClauses();
+			foreach ($catch_clauses as $catch_clause) {
+				$potentially_caught_types = $this->catch_clause_type_resolver->getCaughtTypesForClause($catch_clause);
+				$actually_caught_types = array_intersect($inclosed_encounters, $potentially_caught_types);
+				$this->catch_clause_catches[$catch_clause] = array_values($actually_caught_types);
+				$inclosed_encounters = array_diff($inclosed_encounters, $actually_caught_types); //remove exceptions that are caught
+			}
+			$uncaught = array_merge($uncaught, $inclosed_encounters);
+
+			$this->guarded_scopes[$guarded_scope] = $uncaught;
 		}
-		$this->scopes[$scope] = $uncaught;
+
+		$this->scopes[$scope] = array_values(array_unique($uncaught));
 	}
 
-	public function leaveGuardedScope(GuardedScope $guarded_scope) {
-		$inclosed_encounters = $this->encounters_calculator->getForScope($guarded_scope->getInclosedScope());
-
-		foreach ($guarded_scope->getCatchClauses() as $catch_clause) {
-			$potentially_caught_types = $this->catch_clause_type_resolver->getCaughtTypesForClause($catch_clause);
-
-			$actually_caught_types = array_intersect($inclosed_encounters, $potentially_caught_types);
-			$this->catch_clause_catches[$catch_clause] = $actually_caught_types;
-			$inclosed_encounters = array_diff($inclosed_encounters, $actually_caught_types);
+	public function getCaughtExceptions(Catch_ $catch_clause) {
+		if ($this->catch_clause_catches->contains($catch_clause) === false) {
+			throw new \UnexpectedValueException("Given catch clause could not be found in this set.");
 		}
-		//after looping over all catch clauses, $inclosed_encounters contains all exceptions that have not been caught.
-		$this->guarded_scopes[$guarded_scope] = $inclosed_encounters;
+		return $this->catch_clause_catches[$catch_clause];
 	}
 
-
-
-
-	private function getForGuardedScope(GuardedScope $guarded_scope) {
+	/**
+	 * @param GuardedScope $guarded_scope
+	 * @return Type[] - uncaught types for the
+	 * @throws \UnexpectedValueException
+	 */
+	public function getForGuardedScope(GuardedScope $guarded_scope) {
 		if ($this->guarded_scopes->contains($guarded_scope) === false) {
-			throw new \UnexpectedValueException(sprintf("Guarded Scope enclosed by Scope %s could not be found in this set.", $guarded_scope->getEnclosingScope()->getName()));
+			throw new \UnexpectedValueException(sprintf("Given guarded scope (enclosed by scope with name %s) could not be found in this set.", $guarded_scope->getEnclosingScope()->getName()));
 		}
 		return $this->guarded_scopes[$guarded_scope];
 	}
---
+++ test/PhpExceptionFlow/FlowCalculator/UncaughtCalculatorTest.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace PhpExceptionFlow\FlowCalculator;
+
+use PhpExceptionFlow\GuardedScope;
+use PhpExceptionFlow\Scope;
+use PhpExceptionFlow\ScopeVisitor\CaughtExceptionTypesCalculator;
+use PhpParser\Node\Stmt\Catch_;
+
+class UncaughtCalculatorTest extends \PHPUnit_Framework_TestCase {
+	/** @var UncaughtCalculator $uncaught_calculator */
+	private $uncaught_calculator;
+	/** @var CaughtExceptionTypesCalculator $catch_clause_type_resolver */
+	private $catch_clause_type_resolver;
+	/** @var CombiningCalculator $encounters_calculator */
+	private $encounters_calculator;
+
+	public function setUp() {
+		$this->catch_clause_type_resolver = $this->createMock(CaughtExceptionTypesCalculator::class);
+		$this->encounters_calculator = $this->createMock(CombiningCalculator::class);
+		$this->uncaught_calculator = new UncaughtCalculator($this->catch_clause_type_resolver, $this->encounters_calculator);
+	}
+
+	public function testGetType() {
+		$this->assertEquals("uncaught", $this->uncaught_calculator->getType());
+	}
+
+	public function testThrowsExceptionOnUndeterminedScope() {
+		$scope_mock = $this->createMock(Scope::class);
+		$this->expectException(\UnexpectedValueException::class);
+		$this->uncaught_calculator->getForScope($scope_mock);
+	}
+
+	public function testUncaughtCorrectlyModeledWithOneGuardedScopeWithOneCatchClause() {
+		$enclosing_scope_mock = $this->createMock(Scope::class);
+		$guarded_scope_mock = $this->createMock(GuardedScope::class);
+		$inclosed_scope_mock = $this->createMock(Scope::class);
+		$catch_clause_mock = $this->createMock(Catch_::class);
+
+		$enclosing_scope_mock->expects($this->once())
+			->method("getGuardedScopes")
+			->willReturn(array($guarded_scope_mock));
+
+		$guarded_scope_mock->expects($this->once())
+			->method("getInclosedScope")
+			->willReturn($inclosed_scope_mock);
+		$guarded_scope_mock->expects($this->once())
+			->method("getCatchClauses")
+			->willReturn(array($catch_clause_mock));
+
+		$this->catch_clause_type_resolver->expects($this->once())
+			->method("getCaughtTypesForClause")
+			->with($catch_clause_mock)
+			->willReturn(array("a", "b"));
+
+		$this->encounters_calculator->expects($this->once())
+			->method("getForScope")
+			->with($inclosed_scope_mock)
+			->willReturn(array("b", "c"));
+
+		$this->uncaught_calculator->determineForScope($enclosing_scope_mock);
+		$this->assertEquals(array("c"), $this->uncaught_calculator->getForScope($enclosing_scope_mock));
+		$this->assertEquals(array("c"), $this->uncaught_calculator->getForGuardedScope($guarded_scope_mock));
+		$this->assertEquals(array("b"), $this->uncaught_calculator->getCaughtExceptions($catch_clause_mock));
+	}
+
+	public function testUncaughtCorrectlyModeledWithMultipleGuardedScopesWithMultipleCatchClauses() {
+		$enclosing_scope_mock = $this->createMock(Scope::class);
+		$guarded_scope_1_mock = $this->createMock(GuardedScope::class);
+		$guarded_scope_2_mock = $this->createMock(GuardedScope::class);
+		$inclosed_scope_1_mock = $this->createMock(Scope::class);
+		$inclosed_scope_2_mock = $this->createMock(Scope::class);
+		$catch_clause_1_1_mock = $this->createMock(Catch_::class);
+		$catch_clause_1_2_mock = $this->createMock(Catch_::class);
+		$catch_clause_2_1_mock = $this->createMock(Catch_::class);
+
+		$enclosing_scope_mock->expects($this->once())
+			->method("getGuardedScopes")
+			->willReturn(array($guarded_scope_1_mock, $guarded_scope_2_mock));
+
+		//construct guarded scope 1, with two catch clauses
+		$guarded_scope_1_mock->expects($this->once())
+			->method("getInclosedScope")
+			->willReturn($inclosed_scope_1_mock);
+		$guarded_scope_1_mock->expects($this->once())
+			->method("getCatchClauses")
+			->willReturn(array($catch_clause_1_1_mock, $catch_clause_1_2_mock));
+
+		//construct guarded scope 2, with one catch clause
+		$guarded_scope_2_mock->expects($this->once())
+			->method("getInclosedScope")
+			->willReturn($inclosed_scope_2_mock);
+		$guarded_scope_2_mock->expects($this->once())
+			->method("getCatchClauses")
+			->willReturn(array($catch_clause_2_1_mock));
+
+
+		// set the encounters for the two inclosed scopes
+		$this->encounters_calculator->expects($this->exactly(2))
+			->method("getForScope")
+			->withConsecutive(
+				array($inclosed_scope_1_mock),
+				array($inclosed_scope_2_mock))
+			->will($this->onConsecutiveCalls(
+				array("b", "c", "a"),
+				array("b")
+			));
+
+
+		// set the types that each exception catches
+		$this->catch_clause_type_resolver->expects($this->exactly(3))
+			->method("getCaughtTypesForClause")
+			->withConsecutive(
+				array($catch_clause_1_1_mock),
+				array($catch_clause_1_2_mock),
+				array($catch_clause_2_1_mock))
+			->will($this->onConsecutiveCalls(
+				array("b"),
+				array("a", "b", "c"),
+				array("c")
+			));
+
+		$this->uncaught_calculator->determineForScope($enclosing_scope_mock);
+		$this->assertEquals(array("b"), $this->uncaught_calculator->getForScope($enclosing_scope_mock)); //from second guarded scope
+		$this->assertEquals(array(), $this->uncaught_calculator->getForGuardedScope($guarded_scope_1_mock));
+		$this->assertEquals(array("b"), $this->uncaught_calculator->getForGuardedScope($guarded_scope_2_mock));
+
+		$this->assertEquals(array("b"), $this->uncaught_calculator->getCaughtExceptions($catch_clause_1_1_mock));
+		$this->assertEquals(array("c", "a"), $this->uncaught_calculator->getCaughtExceptions($catch_clause_1_2_mock));
+		$this->assertEquals(array(), $this->uncaught_calculator->getCaughtExceptions($catch_clause_2_1_mock));
+	}
+}
