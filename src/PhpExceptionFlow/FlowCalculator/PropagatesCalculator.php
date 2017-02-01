aab7a722addcc8ff9073f8317ac9a262d451ad20
Tom den Braber <tomdenbraber@gmail.com>
2017-02-01 15:06:09 +0100

    created propagates calculator

---
+++ src/PhpExceptionFlow/FlowCalculator/PropagatesCalculator.php
@@ -0,0 +1,37 @@
+<?php
+namespace PhpExceptionFlow\FlowCalculator;
+
+use PhpExceptionFlow\Scope;
+
+class PropagatesCalculator extends AbstractMutableFlowCalculator {
+	/** @var \SplObjectStorage|Scope[][]  */
+	private $scope_calls_scopes;
+
+	/** @var FlowCalculatorInterface $encounters_calculator */
+	private $encounters_calculator;
+
+	public function __construct(\SplObjectStorage $scope_calls_scopes, FlowCalculatorInterface $encounters_calculator) {
+		parent::__construct();
+		$this->scope_calls_scopes = $scope_calls_scopes;
+		$this->encounters_calculator = $encounters_calculator;
+	}
+
+	public function determineForScope(Scope $scope) {
+		$imported_exceptions = [];
+		if ($this->scope_calls_scopes->contains($scope) === true) {
+			$called_scopes = $this->scope_calls_scopes[$scope];
+			$imported_exceptions = [];
+			/** @var Scope $called_scope */
+			foreach ($called_scopes as $called_scope) {
+				$callee_encounters = $this->encounters_calculator->getForScope($called_scope);
+				$imported_exceptions = array_merge($imported_exceptions, $callee_encounters);
+			}
+			$imported_exceptions = array_values(array_unique($imported_exceptions));
+		}
+		$this->scopes[$scope] = $imported_exceptions;
+	}
+
+	public function getType() {
+		return "propagates";
+	}
+}
---
+++ test/PhpExceptionFlow/FlowCalculator/PropagatesCalculatorTest.php
@@ -0,0 +1,88 @@
+<?php
+namespace PhpExceptionFlow\FlowCalculator;
+
+use PhpExceptionFlow\EncountersCalculator;
+use PhpExceptionFlow\Scope;
+
+class PropagatesCalculatorTest extends \PHPUnit_Framework_TestCase {
+
+	private $encounters_calculator;
+
+	public function setUp() {
+		$this->encounters_calculator = $this->createMock(CombiningCalculator::class);
+
+	}
+
+
+	public function testGetType() {
+		$scope_calls_scopes = new \SplObjectStorage;
+		$this->encounters_calculator->expects($this->never())
+			->method("getForScope");
+		$propagates = new PropagatesCalculator($scope_calls_scopes, $this->encounters_calculator);
+		$this->assertEquals("propagates", $propagates->getType());
+	}
+
+	public function testWithNoCallsReturnsEmpty() {
+		$scope_calls_scopes = new \SplObjectStorage;
+		$this->encounters_calculator->expects($this->never())
+			->method("getForScope");
+		$scope_mock = $this->createScopeMock("a");
+
+		$propagates = new PropagatesCalculator($scope_calls_scopes, $this->encounters_calculator);
+		$propagates->determineForScope($scope_mock);
+
+		$this->assertEmpty($propagates->getForScope($scope_mock));
+	}
+
+	public function testWithCallsThatDoNotEncounterReturnsEmpty() {
+		$caller = $this->createScopeMock("a");
+		$callee1 = $this->createScopeMock("b");
+		$callee2 = $this->createScopeMock("c");
+
+		$scope_calls_scopes = new \SplObjectStorage;
+		$scope_calls_scopes->attach($caller, array($callee1, $callee2));
+
+		$this->encounters_calculator->expects($this->exactly(2))
+			->method("getForScope")
+			->withConsecutive(array($callee1), array($callee2))
+			->willReturn(array());
+
+		$propagates = new PropagatesCalculator($scope_calls_scopes, $this->encounters_calculator);
+		$propagates->determineForScope($caller);
+
+		$this->assertEmpty($propagates->getForScope($caller));
+	}
+
+	public function testWithCallsThatDoEncounterReturnsCorrectExceptions() {
+		$caller = $this->createScopeMock("a");
+		$callee1 = $this->createScopeMock("b");
+		$callee2 = $this->createScopeMock("c");
+
+		$scope_calls_scopes = new \SplObjectStorage;
+		$scope_calls_scopes->attach($caller, array($callee1, $callee2));
+
+		$this->encounters_calculator->expects($this->exactly(2))
+			->method("getForScope")
+			->withConsecutive(array($callee1), array($callee2))
+			->will($this->onConsecutiveCalls(array("x"), array("x", "y")));
+
+		$propagates = new PropagatesCalculator($scope_calls_scopes, $this->encounters_calculator);
+
+		$propagates->determineForScope($caller);
+		$this->assertEquals(array("x", "y"), $propagates->getForScope($caller));
+	}
+
+
+
+
+	private function createScopeMock($name) {
+		$scope_mock = $this->createMock(Scope::class);
+		$scope_mock->method("getName")
+			->willReturn($name);
+		return $scope_mock;
+	}
+
+
+
+
+}
