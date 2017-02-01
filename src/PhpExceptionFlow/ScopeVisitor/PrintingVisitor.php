4b3011bdefb1a86dcc3bd0a8bff8cdb9bf3b81b9
Tom den Braber <tomdenbraber@gmail.com>
2017-02-01 14:20:07 +0100

    print also potentially caught types, update tests accordingly

--- src/PhpExceptionFlow/ScopeVisitor/PrintingVisitor.php
+++ src/PhpExceptionFlow/ScopeVisitor/PrintingVisitor.php
@@ -1,7 +1,7 @@
 <?php
 namespace PhpExceptionFlow\ScopeVisitor;
 
-use PhpExceptionFlow\FlowCalculator\MutableCombiningCalculator;
+use PhpExceptionFlow\FlowCalculator\CombiningCalculatorInterface;
 use PhpExceptionFlow\FlowCalculator\UncaughtCalculator;
 use PhpExceptionFlow\GuardedScope;
 use PhpExceptionFlow\Scope;
@@ -11,15 +11,15 @@ class PrintingVisitor extends AbstractScopeVisitor {
 	private $current_top_level_scope = "";
 	private $result = "";
 
-	/** @var  MutableCombiningCalculator $encounters_calculator */
+	/** @var  CombiningCalculatorInterface $encounters_calculator */
 	private $encounters_calculator;
 	/** @var UncaughtCalculator $uncaught_calculator */
 	private $uncaught_calculator;
 
 
-	public function __construct(MutableCombiningCalculator $encounters_calculator) {
+	public function __construct(CombiningCalculatorInterface $encounters_calculator, UncaughtCalculator $uncaught_calculator) {
 		$this->encounters_calculator = $encounters_calculator;
-		$this->uncaught_calculator = $encounters_calculator->getCalculator("uncaught");
+		$this->uncaught_calculator = $uncaught_calculator;
 	}
 
 	public function beforeTraverse(array $scopes) {
@@ -33,7 +33,7 @@ class PrintingVisitor extends AbstractScopeVisitor {
 		}
 
 		$encounters = $this->encounters_calculator->getForScope($scope);
-
+		print $scope->getName() . " - [" . implode(", ", $encounters) . "]\n";
 		$this->result .= $this->indent . "\t" . "encounters: [" . implode(", ", $encounters) . "]\n";
 		$this->indent .= "\t";
 	}
@@ -48,7 +48,12 @@ class PrintingVisitor extends AbstractScopeVisitor {
 
 	public function leaveGuardedScope(GuardedScope $guarded_scope) {
 		foreach ($guarded_scope->getCatchClauses() as $i => $catch_clause) {
-			$this->result .= $this->indent . "catch#" . $i . " catches: [" . implode(", ",$this->uncaught_calculator->getCaughtExceptions($catch_clause)) . "]\n";
+			$this->result .= sprintf("%scatch#%d catches: [%s], could catch: [%s]\n",
+				$this->indent,
+				$i,
+				implode(", ", $this->uncaught_calculator->getCaughtExceptions($catch_clause)),
+				implode(", ", $this->uncaught_calculator->getPotentiallyCaughtTypes($catch_clause))
+			);
 		}
 		$this->result .= $this->indent . "uncaught: [" . implode(", ", $this->uncaught_calculator->getForGuardedScope($guarded_scope)) . "]\n";
 	}
