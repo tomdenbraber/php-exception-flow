92671f6a558d8a2064b7e6888e9291947baac23b
Tom den Braber <tomdenbraber@gmail.com>
2017-02-07 14:07:26 +0100

    improved exception messages a little bit

--- src/PhpExceptionFlow/CallGraphConstruction/ParserCallNodeToScopeResolver.php
+++ src/PhpExceptionFlow/CallGraphConstruction/ParserCallNodeToScopeResolver.php
@@ -86,7 +86,7 @@ class ParserCallNodeToScopeResolver implements CallResolverInterface {
 					throw new \UnexpectedValueException(sprintf("Method %s->%s() could not be found in method scopes", $class, $call->name));
 				}
 			} else {
-				throw new \UnexpectedValueException(sprintf("Method %s->%s() could not be found in applies to set (%d) ", $class, $call->name, $call->getLine()));
+				throw new \UnexpectedValueException(sprintf("Method %s->%s() could not be found in applies to set (%d) ", $class, is_string($call->name) === true ? $call->name : $call->name->getType(), $call->getLine()));
 			}
 		} else {
 			throw new \UnexpectedValueException(sprintf("Cannot resolve method call; var %s has type %s, method-name is %s (%d)", $call->var->getType(), $type, $call->name, $call->getLine()));
@@ -112,7 +112,7 @@ class ParserCallNodeToScopeResolver implements CallResolverInterface {
 					throw new \UnexpectedValueException(sprintf("Method %s::%s() could not be found in method scopes", $class, $call->name));
 				}
 			} else {
-				throw new \UnexpectedValueException(sprintf("Method %s::%s() could not be found in applies to set", $class, $call->name));
+				throw new \UnexpectedValueException(sprintf("Method %s->%s() could not be found in applies to set (%d) ", $class, is_string($call->name) === true ? $call->name : $call->name->getType(), $call->getLine()));
 			}
 		} else {
 			throw new \UnexpectedValueException(sprintf("Cannot resolve static call; class expression has type %s, method-name is %s", $call->class->getAttribute("type", Type::unknown()), $call->name));
