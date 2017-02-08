2528b9c43907d43d94be905965e1717f9d9c568a
Tom den Braber <tomdenbraber@gmail.com>
2017-02-08 09:47:04 +0100

    added tests for resolving a method to multiple scopes

--- test/PhpExceptionFlow/CallGraphConstruction/ParserCallNodeToScopeResolverTest.php
+++ test/PhpExceptionFlow/CallGraphConstruction/ParserCallNodeToScopeResolverTest.php
@@ -15,14 +15,27 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 	private $method_c_m;
 	private $method_e_m;
 	private $method_f_k;
+	private $method_i_m;
+	private $method_i_n;
+	private $method_abs_m;
+	private $method_abs_n;
+	private $method_conc_m;
+	private $method_conc_n;
 	private $scope_a_m;
 	private $scope_c_m;
 	private $scope_e_m;
 	private $scope_f_k;
 	private $scope_fn_m;
 	private $scope_fn_k;
+	private $scope_i_m;
+	private $scope_i_n;
+	private $scope_abs_m;
+	private $scope_abs_n;
+	private $scope_conc_m;
+	private $scope_conc_n;
 
-	private $applies_to;
+
+	private $class_method_to_actual_implementation;
 	private $function_scopes;
 	private $method_scopes;
 
@@ -31,11 +44,11 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 
 	/**
 	 * Class hierarchy used:
-	 *          a[m]           e[m]
-	 *        /  \             |
-	 *        b   c[m]         f[k]
-	 *            |
-	 *            d
+	 *          a[m]           e[m]            i[m, n]
+	 *        /  \             |                |
+	 *        b   c[m]         f[k]            abs [m, impl[n]]
+	 *            |                             |
+	 *            d                             conc [m, n]
 	 *
 	 * on top of that, functions k and m are defined.
 	 */
@@ -45,33 +58,58 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 		$this->method_e_m = $this->buildMethodMock("e", "m");
 		$this->method_f_k = $this->buildMethodMock("f", "k");
 
+		$this->method_i_m = $this->buildMethodMock("i", "m", false);
+		$this->method_i_n = $this->buildMethodMock("i", "n", false);
+		$this->method_abs_m = $this->buildMethodMock("abs", "m", false);
+		$this->method_abs_n = $this->buildMethodMock("abs", "n", true);
+		$this->method_conc_m = $this->buildMethodMock("conc", "m", true);
+		$this->method_conc_n = $this->buildMethodMock("conc", "n", true);
+
 		$this->scope_a_m = $this->buildScopeMock("a::m");
 		$this->scope_c_m = $this->buildScopeMock("c::m");
 		$this->scope_e_m = $this->buildScopeMock("e::m");
 		$this->scope_f_k = $this->buildScopeMock("f::k");
+		$this->scope_i_m = $this->buildScopeMock("i::m");
+		$this->scope_i_n = $this->buildScopeMock("i::n");
+		$this->scope_abs_m = $this->buildScopeMock("abs::m");
+		$this->scope_abs_n = $this->buildScopeMock("abs::n");
+		$this->scope_conc_m = $this->buildScopeMock("conc::m");
+		$this->scope_conc_n = $this->buildScopeMock("conc::n");
 		$this->scope_fn_k = $this->buildScopeMock("k");
 		$this->scope_fn_m = $this->buildScopeMock("m");
 
-		$this->applies_to = [
+		$this->class_method_to_actual_implementation = [
 			"a" => [
-				"m" => $this->method_a_m,
+				"m" => [$this->method_a_m],
 			],
 			"b" => [
-				"m" => $this->method_a_m,
+				"m" => [$this->method_a_m],
 			],
 			"c" => [
-				"m" => $this->method_c_m,
+				"m" => [$this->method_c_m],
 			],
 			"d" => [
-				"m" => $this->method_c_m,
+				"m" => [$this->method_c_m],
 			],
 			"e" => [
-				"m" => $this->method_e_m,
+				"m" => [$this->method_e_m],
 			],
 			"f" => [
-				"m" => $this->method_e_m,
-				"k" => $this->method_f_k,
-			]
+				"m" => [$this->method_e_m],
+				"k" => [$this->method_f_k],
+			],
+			"i" => [
+				"m" => [$this->method_conc_m],
+				"n" => [$this->method_abs_n, $this->method_conc_n],
+			],
+			"abs" => [
+				"m" => [$this->method_conc_m],
+				"n" => [$this->method_abs_n, $this->method_conc_n],
+			],
+			"conc" => [
+				"m" => [$this->method_conc_m],
+				"n" => [$this->method_conc_n],
+			],
 		];
 		$this->function_scopes = array(
 			"k" => $this->scope_fn_k,
@@ -89,9 +127,22 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 			],
 			"f" => [
 				"k" => $this->scope_f_k,
-			]
+			],
+
+			"i" => [
+				"m" => $this->scope_i_m,
+				"n" => $this->scope_i_n,
+			],
+			"abs" => [
+				"m" => $this->scope_abs_m,
+				"n" => $this->scope_abs_n,
+			],
+			"conc" => [
+				"m" => $this->scope_conc_m,
+				"n" => $this->scope_conc_n,
+			],
 		);
-		$this->resolver = new ParserCallNodeToScopeResolver($this->method_scopes, $this->function_scopes, $this->applies_to);
+		$this->resolver = new ParserCallNodeToScopeResolver($this->method_scopes, $this->function_scopes, $this->class_method_to_actual_implementation);
 	}
 
 	public function testThrowsLogicExceptionOnWrongNodeType() {
@@ -114,7 +165,7 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 
 		$method_call_a_m = new MethodCall($var_expr, "m");
 		$resolved_to = $this->resolver->resolve($method_call_a_m);
-		$this->assertEquals($this->scope_a_m, $resolved_to);
+		$this->assertEquals([$this->scope_a_m], $resolved_to);
 	}
 
 	public function testMethodCallToMethodInSubclassOfImplementingClass() {
@@ -125,7 +176,7 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 
 		$method_call_b_m = new MethodCall($var_expr, "m");
 		$resolved_to = $this->resolver->resolve($method_call_b_m);
-		$this->assertEquals($this->scope_a_m, $resolved_to);
+		$this->assertEquals([$this->scope_a_m], $resolved_to);
 	}
 
 	public function testMethodCallToClassWhichOverridesAMethod() {
@@ -136,7 +187,7 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 
 		$method_call_c_m = new MethodCall($var_expr, "m");
 		$resolved_to = $this->resolver->resolve($method_call_c_m);
-		$this->assertEquals($this->scope_c_m, $resolved_to);
+		$this->assertEquals([$this->scope_c_m], $resolved_to);
 	}
 
 	public function testMethodCallToSameNameButDifferentHierarchy() {
@@ -147,7 +198,7 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 
 		$method_call_e_m = new MethodCall($var_expr, "m");
 		$resolved_to = $this->resolver->resolve($method_call_e_m);
-		$this->assertEquals($this->scope_e_m, $resolved_to);
+		$this->assertEquals([$this->scope_e_m], $resolved_to);
 	}
 
 	public function testMethodCallToMethodWithSameNameAsFunction() {
@@ -158,19 +209,19 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 
 		$method_call_f_k = new MethodCall($var_expr, "k");
 		$resolved_to = $this->resolver->resolve($method_call_f_k);
-		$this->assertEquals($this->scope_f_k, $resolved_to);
+		$this->assertEquals([$this->scope_f_k], $resolved_to);
 	}
 
 	public function testStaticCallToMethodInSubclassOfImplementingClass() {
 		$method_call_b_m = new StaticCall(new Name("b"), "m");
 		$resolved_to = $this->resolver->resolve($method_call_b_m);
-		$this->assertEquals($this->scope_a_m, $resolved_to);
+		$this->assertEquals([$this->scope_a_m], $resolved_to);
 	}
 
 	public function testStaticCallToClassWhichOverridesAMethod() {
 		$method_call_c_m = new StaticCall(new Name("c"), "m");
 		$resolved_to = $this->resolver->resolve($method_call_c_m);
-		$this->assertEquals($this->scope_c_m, $resolved_to);
+		$this->assertEquals([$this->scope_c_m], $resolved_to);
 	}
 	public function testCallToNonExistingMethodThrowsException() {
 		$var_expr = $this->createMock(\PhpParser\Node\Expr::class);
@@ -179,11 +230,43 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 			->willReturn(new Type(Type::TYPE_OBJECT, array(), "f"));
 
 		$method_call_f_k = new MethodCall($var_expr, "g");
-		$this->expectException(\LogicException::class);
+		$this->expectException(\UnexpectedValueException::class);
 		$this->expectExceptionMessage("Method f->g() could not be found in applies to set");
 		$this->resolver->resolve($method_call_f_k);
 	}
 
+	public function testCallToInterfaceMethodResolvesToAllConcreteImplementingMethods() {
+		$var_expr = $this->createMock(\PhpParser\Node\Expr::class);
+		$var_expr->method("getAttribute")
+			->with("type", $this->anything())
+			->willReturn(new Type(Type::TYPE_OBJECT, array(), "i")); //i is an interface
+
+		$method_call_i_m = new MethodCall($var_expr, "n");
+		$resolved_to = $this->resolver->resolve($method_call_i_m);
+		$this->assertEquals([$this->scope_abs_n, $this->scope_conc_n], $resolved_to);
+	}
+
+	public function testCallToAbstractMethodResolvesToAllConcreteImplementingMethods() {
+		$var_expr = $this->createMock(\PhpParser\Node\Expr::class);
+		$var_expr->method("getAttribute")
+			->with("type", $this->anything())
+			->willReturn(new Type(Type::TYPE_OBJECT, array(), "abs")); //abs is an abstract class
+
+		$method_call_i_m = new MethodCall($var_expr, "m"); //m is an abstract method
+		$resolved_to = $this->resolver->resolve($method_call_i_m);
+		$this->assertEquals([$this->scope_conc_m], $resolved_to);
+	}
+
+	public function testCallToInterfaceMethodWithBothAbstractAndConcreteMethodResolvesToConcrete() {
+		$var_expr = $this->createMock(\PhpParser\Node\Expr::class);
+		$var_expr->method("getAttribute")
+			->with("type", $this->anything())
+			->willReturn(new Type(Type::TYPE_OBJECT, array(), "i")); //i is an interface
+
+		$method_call_i_m = new MethodCall($var_expr, "m"); //i->m: interface, abs->m: abstract, so should resolve to conc->m
+		$resolved_to = $this->resolver->resolve($method_call_i_m);
+		$this->assertEquals([$this->scope_conc_m], $resolved_to);
+	}
 
 
 	/**
@@ -192,10 +275,11 @@ class ParserCallNodeToScopeResolverTest extends \PHPUnit_Framework_TestCase {
 	 * @throws \PHPUnit_Framework_Exception
 	 * @return Method
 	 */
-	private function buildMethodMock($class, $method_name) {
+	private function buildMethodMock($class, $method_name, $implemented = true) {
 		$method_mock = $this->createMock(Method::class);
 		$method_mock->method('getClass')->willReturn($class);
 		$method_mock->method('getName')->willReturn($method_name);
+		$method_mock->method('isImplemented')->willReturn($implemented);
 		return $method_mock;
 	}
 
