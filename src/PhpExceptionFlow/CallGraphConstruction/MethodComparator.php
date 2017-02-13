7d32f60b78740dc2d996a46697feb43ca435c3ee
Tom den Braber <tomdenbraber@gmail.com>
2017-02-13 15:36:14 +0100

    add support for traits in method comparator

--- src/PhpExceptionFlow/CallGraphConstruction/MethodComparator.php
+++ src/PhpExceptionFlow/CallGraphConstruction/MethodComparator.php
@@ -2,6 +2,7 @@
 namespace PhpExceptionFlow\CallGraphConstruction;
 
 use PhpExceptionFlow\Collection\PartialOrder\ComparatorInterface;
+use PhpExceptionFlow\Collection\PartialOrder\PartialOrder;
 use PhpExceptionFlow\Collection\PartialOrderInterface;
 use PHPTypes\State;
 
@@ -32,16 +33,69 @@ class MethodComparator implements ComparatorInterface {
 		} else if ($element1->getClass() === $element2->getClass()) {
 			return PartialOrderInterface::EQUAL;
 		} else {
-			$element1_class_resolves = $this->state->classResolves[$element1->getClass()];
-			$element2_class_resolves = $this->state->classResolves[$element2->getClass()];
-
-			if (isset($element1_class_resolves[$element2->getClass()]) === true) {
-				return PartialOrderInterface::SMALLER;
-			} else if (isset($element2_class_resolves[$element1->getClass()]) === true) {
-				return PartialOrderInterface::GREATER;
-			} else {
-				return PartialOrderInterface::NOT_COMPARABLE;
+			if (($this->state->isClass($element1->getClass()) || $this->state->isInterface($element1->getClass())) &&
+				($this->state->isClass($element2->getClass()) || $this->state->isInterface($element2->getClass()))
+			) {
+				return $this->compareClassMethods($element1, $element2);
+			} else if ($this->state->isTrait($element1->getClass()) &&
+				($this->state->isClass($element2->getClass()) || $this->state->isInterface($element2->getClass()))) {
+				return $this->compareTraitAndClassMethod($element1, $element2);
+			} else if (($this->state->isClass($element1->getClass()) || $this->state->isInterface($element1->getClass())) &&
+				$this->state->isTrait($element2->getClass())) {
+				return $this->invert($this->compareTraitAndClassMethod($element2, $element1));
+			} else if ($this->state->isTrait($element1->getClass()) && $this->state->isTrait($element2->getClass())) {
+				// todo: this will result in an error, except if the programmer indicated preference for a certain type within a class.
+				// but this preference can differ per class, so it is not an easy thing to add; my guess is that it does not occur that often.
+				return PartialOrder::NOT_COMPARABLE;
 			}
+		}
+	}
+
+	private function compareClassMethods(Method $element1, Method $element2) {
+		$element1_class_resolves = $this->state->classResolves[$element1->getClass()];
+		$element2_class_resolves = $this->state->classResolves[$element2->getClass()];
+
+		if (isset($element1_class_resolves[$element2->getClass()]) === true) {
+			return PartialOrderInterface::SMALLER;
+		} else if (isset($element2_class_resolves[$element1->getClass()]) === true) {
+			return PartialOrderInterface::GREATER;
+		} else {
+			return PartialOrderInterface::NOT_COMPARABLE;
+		}
+	}
+
+	private function compareTraitAndClassMethod(Method $trait_method, Method $class_method) {
+		$trait_resolves = $this->state->classResolves[$trait_method->getClass()];
+		$class_resolves = $this->state->classResolves[$class_method->getClass()];
+
+		if (isset($trait_resolves[$class_method->getClass()]) === true) {
+			return PartialOrderInterface::SMALLER;
+		} else if (isset($class_resolves[$trait_method->getClass()]) === true) {
+			return PartialOrderInterface::GREATER;
+		} else {
+			// if a subclass of the class resolves to the trait, the trait method is smaller.
+			foreach ($this->state->classResolvedBy[$trait_method->getClass()] as $class_using_trait) {
+				if (isset($this->state->classResolvedBy[$class_method->getClass()][$class_using_trait]) === true) {
+					return PartialOrderInterface::SMALLER;
+				} else if (isset($this->state->classResolvedBy[$class_using_trait][$class_method->getClass()]) === true) {
+					return PartialOrder::GREATER;
+				}
+			}
+			return PartialOrderInterface::NOT_COMPARABLE;
+		}
+	}
+
+	private function compareTraitMethods(Method $trait_method1, Method $trait_method2) {
+
+	}
+
+	private function invert(int $comparison_outcome) {
+		if ($comparison_outcome === PartialOrder::GREATER) {
+			return PartialOrder::SMALLER;
+		} else if ($comparison_outcome === PartialOrder::SMALLER) {
+			return PartialOrder::GREATER;
+		} else {
+			return $comparison_outcome;
 		}
 	}
 }
--- test/PhpExceptionFlow/CallGraphConstruction/MethodComparatorTest.php
+++ test/PhpExceptionFlow/CallGraphConstruction/MethodComparatorTest.php
@@ -9,42 +9,6 @@ use PHPTypes\State;
 
 
 class MethodComparatorTest extends \PHPUnit_Framework_TestCase {
-	/**
-	 * @var array
-	 * representing the following class hierarchy:
-	 *          a           f
-	 *        /   \
-	 *       b     c
-	 *     /  \
-	 *   d     e
-	 */
-	private $resolves = [
-		"a" => [
-			"a" => "a",
-		],
-		"f" => [
-			"f" => "f",
-		],
-		"b" => [
-			"a" => "a",
-			"b" => "b",
-		],
-		"c" => [
-			"a" => "a",
-			"c" => "c"
-		],
-		"d" => [
-			"a" => "a",
-			"b" => "b",
-			"d" => "d"
-		],
-		"e" => [
-			"a" => "a",
-			"b" => "b",
-			"e" => "e"
-		],
-	];
-
 	private $state;
 
 	/** @var MethodComparator */
@@ -52,12 +16,12 @@ class MethodComparatorTest extends \PHPUnit_Framework_TestCase {
 
 	/** @var ClassMethod */
 	private $method_m;
+
 	/** @var ClassMethod */
 	private $method_f;
 
 	public function setUp() {
 		$this->state = $this->createMock(State::class);
-		$this->state->classResolves = $this->resolves;
 		$this->comparator = new MethodComparator($this->state);
 		$this->method_m = new ClassMethod("m");
 		$this->method_f = new ClassMethod("f");
@@ -76,32 +40,202 @@ class MethodComparatorTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	public function testOverridingMethodIsSmaller() {
+		$this->setStateToClassHierarchyWithOnlyClasses();
 		$m1 = new Method("b", $this->method_m);
 		$m2 = new Method("a", $this->method_m);
 		$this->assertEquals(PartialOrder::SMALLER, $this->comparator->compare($m1, $m2));
 	}
 
 	public function testOverriddenMethodIsGreater() {
+		$this->setStateToClassHierarchyWithOnlyClasses();
 		$m1 = new Method("a", $this->method_m);
 		$m2 = new Method("b", $this->method_m);
 		$this->assertEquals(PartialOrder::GREATER, $this->comparator->compare($m1, $m2));
 	}
 
 	public function testOverriddenDeeperInChainMethodIsGreater() {
+		$this->setStateToClassHierarchyWithOnlyClasses();
 		$m1 = new Method("a", $this->method_m);
 		$m2 = new Method("e", $this->method_m);
 		$this->assertEquals(PartialOrder::GREATER, $this->comparator->compare($m1, $m2));
 	}
 
 	public function testSiblingsAreNotComparable() {
+		$this->setStateToClassHierarchyWithOnlyClasses();
 		$m1 = new Method("d", $this->method_m);
 		$m2 = new Method("e", $this->method_m);
 		$this->assertEquals(PartialOrder::NOT_COMPARABLE, $this->comparator->compare($m1, $m2));
 	}
 
 	public function testNonRelatedClassesAreNotComparable() {
+		$this->setStateToClassHierarchyWithOnlyClasses();
 		$m1 = new Method("a", $this->method_m);
 		$m2 = new Method("f", $this->method_m);
 		$this->assertEquals(PartialOrder::NOT_COMPARABLE, $this->comparator->compare($m1, $m2));
+	}
+
+	public function testDerivingClassMethodSmallerThanBaseClassWithTraitInBetween() {
+		$this->setStateToClassHierarchyWithClassesAndTraits();
+		$m1 = new Method("b", $this->method_m);
+		$m2 = new Method("a", $this->method_m);
+		$this->assertEquals(PartialOrder::SMALLER, $this->comparator->compare($m1, $m2));
+	}
+
+	public function testTraitMethodGreaterThanUsingClassMethod() {
+		$this->setStateToClassHierarchyWithClassesAndTraits();
+		$m1 = new Method("t1", $this->method_m);
+		$m2 = new Method("b", $this->method_m);
+		$this->assertEquals(PartialOrder::GREATER, $this->comparator->compare($m1, $m2));
+	}
+
+	public function testClassMethodAndTraitMethodNonRelatedAreNotComparable() {
+		$this->setStateToClassHierarchyWithClassesAndTraits();
+		$m1 = new Method("t1", $this->method_m);
+		$m2 = new Method("c", $this->method_m);
+		$this->assertEquals(PartialOrder::NOT_COMPARABLE, $this->comparator->compare($m1, $m2));
+	}
+
+	public function testMethodInTraitUsedInDerivedClassIsSmallerThanBaseClass() {
+		$this->setStateToClassHierarchyWithClassesAndTraits();
+		$m1 = new Method("t1", $this->method_m);
+		$m2 = new Method("a", $this->method_m);
+		$this->assertEquals(PartialOrder::SMALLER, $this->comparator->compare($m1, $m2));
+	}
+
+	/**
+	 * builds hierarchy representing the following class hierarchy:
+	 *          a           f
+	 *        /   \
+	 *       b     c
+	 *     /  \
+	 *   d     e
+	 */
+	private function setStateToClassHierarchyWithOnlyClasses() {
+		$this->state->method("isClass")->willReturn(true);
+		$this->state->method("isTrait")->willReturn(false);
+		$this->state->method("isInterface")->willReturn(false);
+
+		$this->state->classResolvedBy = [
+			"a" => [
+				"b" => "b",
+				"c" => "c",
+				"d" => "d",
+				"e" => "e",
+			],
+			"b" => [
+				"b" => "b",
+				"d" => "d",
+				"e" => "e",
+			],
+			"c" => [
+				"c" =>  "e",
+			],
+			"d" => [
+				"d" => "d",
+			],
+			"e" => [
+				"e" => "e",
+			],
+			"f" => [
+				"f" => "f",
+			],
+		];
+
+
+		$this->state->classResolves = [
+			"a" => [
+				"a" => "a",
+			],
+			"f" => [
+				"f" => "f",
+			],
+			"b" => [
+				"a" => "a",
+				"b" => "b",
+			],
+			"c" => [
+				"a" => "a",
+				"c" => "c"
+			],
+			"d" => [
+				"a" => "a",
+				"b" => "b",
+				"d" => "d"
+			],
+			"e" => [
+				"a" => "a",
+				"b" => "b",
+				"e" => "e"
+			],
+		];
+	}
+
+	/**
+	 *         a          traits: t1, t2
+	 *    /        \
+	 * b[t1]       c[t1]
+	 */
+	private function setStateToClassHierarchyWithClassesAndTraits() {
+		$is_class_map = [
+			["a", true],
+			["b", true],
+			["c", true],
+			["t1", false],
+			["t2", false],
+		];
+
+		$is_trait_map = [
+			["a", false],
+			["b", false],
+			["c", false],
+			["t1", true],
+			["t2", true],
+		];
+
+		$this->state->method("isClass")->will($this->returnValueMap($is_class_map));
+		$this->state->method("isTrait")->will($this->returnValueMap($is_trait_map));
+
+		$this->state->classResolvedBy = [
+			"a" => [
+				"a" => "a",
+				"b" => "b"
+			],
+			"b" => [
+				"b" => "b"
+			],
+			"c" => [
+				"c" => "c",
+			],
+			"t1" => [
+				"t1" => "t1",
+				"b" => "b",
+			],
+			"t2" => [
+				"t2" => "t2",
+				"c" => "c",
+			],
+		];
+
+		$this->state->classResolves = [
+			"a" => [
+				"a" => "a",
+			],
+			"t1" => [
+				"t1" => "t1"
+			],
+			"t2" => [
+				"t2" => "t2"
+			],
+			"b" => [
+				"b" => "b",
+				"a" => "a",
+				"t1" => "t1",
+			],
+			"c" => [
+				"c" => "c",
+				"a" => "a",
+				"t2" => "t2",
+			]
+		];
 	}
 }
