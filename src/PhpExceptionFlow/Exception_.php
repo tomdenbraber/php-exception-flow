8e64db5c3d48c25526fcf3ebd38c44a38a6f140d
Tom den Braber <tomdenbraber@gmail.com>
2017-03-01 10:43:08 +0100

    added classes for modeling an exception and its propagation path(s)

---
+++ src/PhpExceptionFlow/Exception_.php
@@ -0,0 +1,65 @@
+<?php
+namespace PhpExceptionFlow;
+
+use PhpExceptionFlow\Scope\GuardedScope;
+use PhpExceptionFlow\Scope\Scope;
+use PhpParser\Node;
+use PHPTypes\Type;
+
+class Exception_ {
+	/** @var Type */
+	private $type;
+	/** @var Node\Stmt */
+	private $initial_cause;
+	/** @var PropagationPath[] */
+	private $propagation_paths = [];
+	/** @var GuardedScope[] */
+	private $escaped_guarded_scopes = [];
+
+	public function __construct(Type $type, Node\Stmt $initial_cause, Scope $caused_in) {
+		$this->type = $type;
+		$this->initial_cause = $initial_cause;
+		$this->propagation_paths[] = PropagationPath::fromInitialScope($caused_in);
+	}
+
+	/**
+	 * @return Type
+	 */
+	public function getType() {
+		return $this->type;
+	}
+
+	/**
+	 * @return PropagationPath[]
+	 */
+	public function getPropagationPaths() {
+		return $this->propagation_paths;
+	}
+
+	/**
+	 * @return Node|Node\Stmt
+	 */
+	public function getInitialCause() {
+		return $this->initial_cause;
+	}
+
+	/**
+	 * @return GuardedScope[]
+	 */
+	public function getEscapedGuardedScopes() {
+		return $this->escaped_guarded_scopes;
+	}
+
+	/**
+	 * @param Scope $caller_scope
+	 * @param Scope $callee_scope
+	 */
+	public function propagate(Scope $caller_scope, Scope $callee_scope) {
+		foreach ($this->propagation_paths as $propagation_path) {
+			if ($propagation_path->lastOcccurrencesOfScopesAreCallingEachother($caller_scope, $callee_scope) === true) {
+				$this->propagation_paths[] = $propagation_path->addCall($caller_scope, $callee_scope);
+			}
+
+		}
+	}
+}
---
+++ src/PhpExceptionFlow/PropagationPath.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace PhpExceptionFlow;
+
+use PhpExceptionFlow\Scope\Scope;
+
+class PropagationPath {
+	/** @var Scope[] */
+	private $scope_chain;
+	/**
+	 * @var \SplObjectStorage $scope_to_index
+	 * contains the highest index for each scope in the call chain
+	 */
+	private $scope_to_index;
+
+	/**
+	 * PropagationPath constructor.
+	 * @param Scope[] $scope_chain
+	 * @param \SplObjectStorage $scope_to_index
+	 */
+	private function __construct(array $scope_chain, \SplObjectStorage $scope_to_index) {
+		$this->scope_chain = $scope_chain;
+		$this->scope_to_index = $scope_to_index;
+	}
+
+	/**
+	 * @return Scope[]
+	 */
+	public function getScopeChain() {
+		return $this->scope_chain;
+	}
+
+	/**
+	 * For lack of a better name.
+	 * @param Scope $caller
+	 * @param Scope $callee
+	 * @return bool
+	 */
+	public function lastOcccurrencesOfScopesAreCallingEachother(Scope $caller, Scope $callee) {
+		return $this->scope_to_index[$callee] === $this->scope_to_index[$caller] + 1;
+	}
+
+	/**
+	 * @param Scope $from
+	 * @param Scope $to
+	 * @return PropagationPath
+	 */
+	public function addCall(Scope $from, Scope $to) {
+		$caller_index = $this->scope_to_index[$from];
+		$new_scope_chain = array_slice($this->scope_chain, 0, $caller_index + 1);
+		$new_scope_to_index = new \SplObjectStorage;
+
+		foreach ($new_scope_chain as $scope) {
+			$new_scope_to_index->attach($scope, $this->scope_to_index[$scope]);
+		}
+
+		$new_scope_chain[] = $to;
+		$new_scope_to_index[$to] = $caller_index + 1;
+
+		return new PropagationPath($new_scope_chain, $new_scope_to_index);
+	}
+
+	/**
+	 * @param Scope $scope
+	 * @return PropagationPath
+	 */
+	public static function fromInitialScope(Scope $scope) {
+		$scope_chain = [$scope];
+		$scope_to_index = new \SplObjectStorage;
+		$scope_to_index->attach($scope, 0);
+		return new PropagationPath($scope_chain, $scope_to_index);
+	}
+}
