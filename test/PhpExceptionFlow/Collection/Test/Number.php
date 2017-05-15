ee640fb8199dffe39b510719659ed628f69f958f
Tom den Braber <tomdenbraber@gmail.com>
2017-05-15 16:29:18 +0200

    let testclass Number implement PartialOrderElementInterface to fix tests

--- test/PhpExceptionFlow/Collection/Test/Number.php
+++ test/PhpExceptionFlow/Collection/Test/Number.php
@@ -2,10 +2,16 @@
 namespace PhpExceptionFlow\Collection\Test;
 
 //this class is only used for testing the PartialOrder class
-class Number {
+use PhpExceptionFlow\Collection\PartialOrder\PartialOrderElementInterface;
+
+class Number implements PartialOrderElementInterface {
 	public $value;
 
 	public function __construct($value) {
 		$this->value = $value;
+	}
+
+	public function __toString() {
+		return (string)$this->value;
 	}
 }
